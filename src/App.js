fa3d56853d1ac923bddbb70d9c7a35b4b1fa1f53
Apurv Jha <apurv14jha@gmail.com>
2025-12-02 21:33:47 +0530

    remove unnecessary content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import "./App.css";
-// Option 1: use logo from public folder via absolute URL
-// const logoUrl = "/logo.svg";
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
 
-// Option 2: import logo as a module (uses asset/resource rule)
-import logo from "./logo.svg";
-
-const App = () => (
-  <div className="app-root">
-    <h1>Hello, React!</h1>
-    {/* Using imported logo (Webpack asset/resource) */}
-    <img src={logo} alt="Logo" width="120" />
-    {/* If you prefer public path instead, comment the line above and uncomment below */}
-    {/* <img src={logoUrl} alt="Logo" width="120" /> */}
-  </div>
-);
-
-export default App;
+export default App
