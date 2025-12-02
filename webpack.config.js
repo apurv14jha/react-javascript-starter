019b4454b8f5fa4574c1eb1620b3976aa79b1931
apurv14jha <79317498+apurv14jha@users.noreply.github.com>
2025-12-02 19:52:01 +0530

    Restore standard gitignore defaults

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    mode: argv.mode || "development",
+    devtool: isProd ? "source-map" : "eval-source-map",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[contenthash].js",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "public", to: ".", globOptions: { ignore: ["**/index.html"] } },
+        ],
+      }),
+    ],
+    devServer: {
+      static: "./dist",
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
+};
